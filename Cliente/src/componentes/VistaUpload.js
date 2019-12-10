e8f191cba186342fb87bc3c4e3199ca578d71b33
IFTS 16 <mail@mail.com>
2019-12-10 19:56:50 -0300

    upload image

--- Cliente/src/componentes/VistaUpload.js
+++ Cliente/src/componentes/VistaUpload.js
@@ -7,18 +7,35 @@ export default class VistaUpload extends React.Component{
 
     
     state = {
-        //texto: '',
-        SelectImagen: null
-        //fk_id_user: ''
+        texto: '',
+        SelectImagen: null,
+        fk_id_user: ''
     }
 
-    guardarCampo = event => {
-        this.setState({
-            //texto: event.target.value,
-            //fk_id_user: event.target.value,
-            SelectImagen: event.target.files[0],
-           // loaded: 0,
-        })
+    guardarCampo = campo => event => {
+        if (campo === 'texto') {
+            this.setState({
+                ...this.state,
+                texto: event.target.value,
+            })
+            return
+        }
+
+        if (campo === 'imagen') {
+            this.setState({
+                ...this.state,
+                SelectImagen: event.target.files ? event.target.files[0] : null,
+            })
+            return
+        }
+
+        if (campo === 'fk_id_user') {
+            this.setState({
+                ...this.state,
+                fk_id_user: event.target.value,
+            })
+            return
+        }
 
     }
 
@@ -26,9 +43,9 @@ export default class VistaUpload extends React.Component{
 
         const data = new FormData();
 
-        //data.append('texto', this.state.texto);
+        data.append('texto', this.state.texto);
         data.append('imagen', this.state.SelectImagen);
-        //data.append('fk_id_user', this.state.texto);
+        data.append('fk_id_user', this.state.fk_id_user);
 
         axios.post("http://localhost:4000/api/posteos/",data, {
 
@@ -52,18 +69,18 @@ export default class VistaUpload extends React.Component{
                     <div className="card-body">
                         <form method="POST" encType="multipart/form-data" >
                             <div className="form-group">
-                                <input onChange={this.guardarCampo} type="text" name="texto" placeholder="Texto" className="form-control"></input>
+                                <input onChange={this.guardarCampo('texto')} type="text" name="texto" placeholder="Texto" className="form-control"></input>
                             </div>
                             
                             <div className="input-group mb-3">
                             <div className="custom-file">
-                              <input onChange={this.guardarCampo} type="file" name="imagen" className="custom-file-input" id="inputGroupFile03" aria-describedby="inputGroupFileAddon03"></input>
+                              <input onChange={this.guardarCampo('imagen')} type="file" name="imagen" className="custom-file-input" id="inputGroupFile03" aria-describedby="inputGroupFileAddon03"></input>
                               <label className="custom-file-label" for="inputGroupFile03">Elegir archivo</label>
                            </div>
                             </div>
 
                             <div class="form-group">
-                               <input onChange={this.guardarCampo} type="text" name="fk_id_user" class="form-control" placeholder="usuario"></input>
+                               <input onChange={this.guardarCampo('fk_id_user')} type="text" name="fk_id_user" class="form-control" placeholder="usuario"></input>
                             </div>
                             <div class="form-group">
                                <button onClick={this.enviarForm} type="button" name="submitI" class="btn btn-success btn-block">Postear</button>
